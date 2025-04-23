74eaf38cf29785a9e0886f80c85482730ba7516a
Domingo Pineda <domingoantonio2072@gmail.com>
2025-04-23 12:56:39 -0400

    1.7: unicafe

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,7 +5,16 @@ function App() {
   const [good,setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
+  const puntajeBueno = 1
+  const puntajeNeutral =0 
+  const puntajeMalo = -1
+  const all = good+neutral+bad
+  const positive = (good/all) * 100
+  const promedio = () => {
+    return ((good * puntajeBueno) +
+           (neutral + puntajeNeutral) +
+           (bad * puntajeMalo) / all) 
+  }
   return (
     <>
       <h1>give feedback</h1>
@@ -15,9 +24,12 @@ function App() {
         <button onClick={() => {setBad(bad+1)}}>bad</button>
       </div>
       <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
+      <p>good: {good}</p>
+      <p>neutral: {neutral}</p>
+      <p>bad: {bad}</p>
+      <p>all: {all}</p>
+      <p>average:{isNaN(promedio())? 0: promedio() }</p>
+      <p>positive: {positive} %</p>
     </>
   )
 }
